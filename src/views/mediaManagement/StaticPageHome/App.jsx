eab13d34446e34e5e89ee9d087fa07b6bbc871e6
Tan Junfeng <tanjunfeng@yatang.net>
2017-08-11 18:07:10 +0800

    GA-334

--- src/views/mediaManagement/StaticPageHome/App.jsx
+++ src/views/mediaManagement/StaticPageHome/App.jsx
@@ -163,7 +163,7 @@ class CateGory extends Component {
 
     render() {
         const { getFieldDecorator } = this.props.form;
-        const { findstaticpagelist = {} } = this.props;
+        const { findstaticpagelist = {}, prefixCls } = this.props;
         const {
             data,
             pageNum,
@@ -271,11 +271,13 @@ CateGory.propTypes = {
     fetchStaticPageList: PropTypes.func,
     modifyMediaAddVisible: PropTypes.func,
     form: PropTypes.objectOf(PropTypes.any),
+    prefixCls: PropTypes.string,
     findstaticpagelist: PropTypes.objectOf(PropTypes.any),
     location: PropTypes.objectOf(PropTypes.any),
 }
 
 CateGory.defaultProps = {
-}
+    prefixCls: 'cateGory-line',
+};
 
 export default withRouter(Form.create()(CateGory));
