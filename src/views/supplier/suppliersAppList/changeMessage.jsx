fe066fbbf6291f5777f9edc2d524e985263771a3
shijinhua <shijinhua@yatang.cn>
2017-08-16 10:19:20 +0800

    bug fix

--- src/views/supplier/suppliersAppList/changeMessage.jsx
+++ src/views/supplier/suppliersAppList/changeMessage.jsx
@@ -49,9 +49,12 @@ class ChangeMessage extends PureComponent {
      */
     handleInformationOk() {
         const { visibleData = {} } = this.props;
-        const { providerNo } = visibleData;
+        const { id, providerType } = visibleData;
         this.handleInformationCancel();
-        this.props.history.push(`/suppliersAppList/edit/supplier/${providerNo}`)
+        const editUrl = providerType === '1'
+            ? `/suppliersAppList/edit/supplier/${id}`
+            : `/suppliersAppList/edit/${id}`
+        this.props.history.push(editUrl)
     }
 
     render() {
