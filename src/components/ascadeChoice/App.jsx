cf2ded958560ec5aa0317dacfa2416f82279a905
TaoQiyu <taoqiyu@yatang.cn>
2017-08-23 17:30:57 +0800

    删掉未使用的模块

--- src/components/ascadeChoice/App.jsx
+++ src/components/ascadeChoice/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Input, Form, InputNumber } from 'antd';
+import { Form, InputNumber } from 'antd';
 import { Validator } from '../../util/validator'
 
 const FormItem = Form.Item;
