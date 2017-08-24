b8b948c87c199a939ab6478e9785bb4b22dcaae8
Tan Junfeng <tanjunfeng@yatang.net>
2017-08-24 14:21:55 +0800

    修改银行信息验证（20->30）

--- src/views/supplier/addEdit/App.jsx
+++ src/views/supplier/addEdit/App.jsx
@@ -119,7 +119,6 @@ class AddSupplier extends PureComponent {
 
 AddSupplier.propTypes = {
     match: PropTypes.objectOf(PropTypes.any),
-    fetchSupplierNo: PropTypes.objectOf(PropTypes.any),
     getSupplierDetail: PropTypes.func,
     removeDetailData: PropTypes.func,
     fetchSupplierNo: PropTypes.func
