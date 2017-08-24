5d9786f50cc04687cb457e07f5d74c4d3c84402d
Tan Junfeng <tanjunfeng@yatang.net>
2017-08-24 16:28:36 +0800

    修改银行验证

--- src/views/supplier/addEdit/bankInfo.jsx
+++ src/views/supplier/addEdit/bankInfo.jsx
@@ -235,7 +235,7 @@ class BankInfo extends PureComponent {
                                             {getFieldDecorator('bankAccount', {
                                                 rules: [
                                                     { required: true, message: '请输入银行账号!' },
-                                                    { max: 20, message: '长度超限' },
+                                                    { max: 30, message: '长度超限' },
                                                     {
                                                         validator: (rule, value, callback) => {
                                                             if (!/^[0-9]*$/.test(value)) {
