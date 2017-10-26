71260af84644672128f7d526be23ebcb6398b9d3
tanjunfeng <tanjf@yatang.cn>
2017-10-26 10:35:53 +0800

    merge

--- config/config.test.js
+++ config/config.test.js
@@ -5,12 +5,6 @@
  */
 window.config = {
     apiHost: '/api/sc/',
-<<<<<<< HEAD
-    // apiHost: '/sc/',
-    topMenusLink: `${location.protocol}//sitxcsc.yatang.com.cn/`,
-    loginLink: `${location.protocol}//sitxcsc.yatang.com.cn/login.htm`
-=======
     topMenusLink: `${location.protocol}//sitxcoc.yatang.com.cn/`,
     loginLink: `${location.protocol}//sitxcoc.yatang.com.cn/login.htm`
->>>>>>> bf2c08db4d771e359251413b23c3c2073b40397a
 };
