a0590c87907e32b1c8d510cc31a999368578649a
taoqiyu <taoqiyu@yatang.cn>
2017-11-13 21:18:22 +0800

    废弃 SubCompanies 组件

--- src/container/search/index.jsx
+++ src/container/search/index.jsx
@@ -1,6 +1,5 @@
 import BranchCompany from './BranchCompany';
-import SubCompanies from './SubCompanies';
 import DirectStores from './DirectStores';
 import AddingGoodsByStore from './AddingGoodsByStore';
 
-export { BranchCompany, SubCompanies, DirectStores, AddingGoodsByStore };
+export { BranchCompany, DirectStores, AddingGoodsByStore };
