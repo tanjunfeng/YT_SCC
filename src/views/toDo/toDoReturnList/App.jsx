3bad457ee425b3b5bb661ea4c6977e7e0b3e0bea
tanjunfeng <tanjf@yatang.cn>
2017-11-23 11:08:43 +0800

    merge

--- src/views/toDo/toDoReturnList/App.jsx
+++ src/views/toDo/toDoReturnList/App.jsx
@@ -3,7 +3,7 @@
  * @Description: 采购退货
  * @CreateDate: 2017-10-27 11:23:06
  * @Last Modified by: tanjf
- * @Last Modified time: 2017-11-23 11:05:16
+ * @Last Modified time: 2017-11-23 11:08:14
  */
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
