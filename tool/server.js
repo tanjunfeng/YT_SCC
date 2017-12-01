1df67bb6bd4256e47d1ce06f0147551fe9c15ac6
ljy <30369113@qq.com>
2017-12-01 11:46:19 +0800

    检查

--- tool/server.js
+++ tool/server.js
@@ -20,8 +20,9 @@ if (__TEST__) {
     host = 'sitxcsc-static.yatang.com.cn';
     rules = [{
         pattern: /https?:\/\/[-\w\.]*(?::\d+)?\/(.+)/,
-        responder: 'http://sitxcsc.yatang.com.cn/$1',
+        // responder: 'http://sitxcsc.yatang.com.cn/$1',
         // responder: 'http://172.30.40.23:8080/$1',
+        responder: 'http://172.30.40.217:8082/$1'
     }];
 } else {
     host = 'localhost';
