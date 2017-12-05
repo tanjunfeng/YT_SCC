38df197922c6c4b030a5671c3a3f62c8709671f5
taoqiyu <taoqiyu@yatang.cn>
2017-12-05 10:08:55 +0800

    拆分优惠方式组件为单列组件

--- src/views/promotion/create/DomHelper.jsx
+++ src/views/promotion/create/DomHelper.jsx
@@ -25,38 +25,40 @@ export const getChooseButton = (companies, handleClick) => (
 /**
  * 获取优惠方式
  */
-export const getRuls = (getFieldDecorator, getFieldValue) => (<Row>
-    <Col span={16}>
-        <FormItem label="优惠方式">
-            {getFieldDecorator('rule', {
-                initialValue: '',
-                rules: [{ required: true, message: '请选择优惠方式' }]
-            })(<Select size="default" className="wd-90">
-                <Option key={-1} value="">- 请选择 -</Option>
-                <Option key={0} value="0">折扣百分比</Option>
-                <Option key={1} value="1">折扣金额</Option>
-            </Select>)}
-        </FormItem>
-        {/* 优惠百分比 */}
-        {getFieldValue('rule') === '0' ?
-            <FormItem>
-                {getFieldDecorator('percent', {
-                    initialValue: 95,
-                    rules: [{ required: true, message: '请输入折扣百分比' }]
-                })(<InputNumber className="wd-50" min={0} max={100} step={5} />)} %
+export const getRulesColumn = (getFieldDecorator, getFieldValue) => (<Col span={16}>
+    <FormItem label="优惠方式">
+        {getFieldDecorator('rule', {
+            initialValue: '',
+            rules: [{ required: true, message: '请选择优惠方式' }]
+        })(<Select size="default" className="wd-90">
+            <Option key={-1} value="">- 请选择 -</Option>
+            <Option key={0} value="0">折扣百分比</Option>
+            <Option key={1} value="1">折扣金额</Option>
+        </Select>)}
+    </FormItem>
+    {/* 优惠百分比 */}
+    {getFieldValue('rule') === '0' ?
+        <FormItem>
+            {getFieldDecorator('percent', {
+                initialValue: 95,
+                rules: [{ required: true, message: '请输入折扣百分比' }]
+            })(<InputNumber className="wd-50" min={0} max={100} step={5} />)} %
             </FormItem>
-            : null}
-        {/* 折扣金额 */}
-        {getFieldValue('rule') === '1' ?
-            <FormItem>
-                ￥{getFieldDecorator('amout', {
-                    initialValue: 0,
-                    rules: [
-                        { required: true, message: '请输入折扣金额' },
-                        { validator: Util.limitTwoDecimalPlaces }
-                    ]
-                })(<InputNumber className="wd-60" min={0} max={MAX_AMOUNT_OF_ORDER} step={1} />)} 元
+        : null}
+    {/* 折扣金额 */}
+    {getFieldValue('rule') === '1' ?
+        <FormItem>
+            ￥{getFieldDecorator('amout', {
+                initialValue: 0,
+                rules: [
+                    { required: true, message: '请输入折扣金额' },
+                    { validator: Util.limitTwoDecimalPlaces }
+                ]
+            })(<InputNumber className="wd-60" min={0} max={MAX_AMOUNT_OF_ORDER} step={1} />)} 元
             </FormItem>
-            : null}
-    </Col>
+        : null}
+</Col>)
+
+export const getRules = (getFieldDecorator, getFieldValue) => (<Row>
+    {getRulesColumn(getFieldDecorator, getFieldValue)}
 </Row>)
