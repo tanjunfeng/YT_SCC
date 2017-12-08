0c058cd0b51e800c2589665078fcdfc85caad376
taoqiyu <taoqiyu@yatang.cn>
2017-12-08 17:18:02 +0800

    修复金额 validator 写错的问题

--- src/views/promotion/create/domHelper.jsx
+++ src/views/promotion/create/domHelper.jsx
@@ -68,7 +68,7 @@ const getRulesColumn = (form, licence, type = '') => {
                     initialValue: 0,
                     rules: [
                         { required: true, message: '请输入折扣金额' },
-                        { validat: Util.limitTwoDecimalPlaces }
+                        { validator: Util.limitTwoDecimalPlaces }
                     ]
                 })(<InputNumber className="wd-60" min={0} max={MAX_AMOUNT_OF_ORDER} step={1} />)} 元
             </FormItem>
