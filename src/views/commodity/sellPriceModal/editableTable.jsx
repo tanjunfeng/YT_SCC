e553ffdf08f7bc5174a29ac18469176e449cb1f6
taoqiyu <taoqiyu@yatang.cn>
2017-12-20 15:13:29 +0800

    取数据

--- src/views/commodity/sellPriceModal/editableTable.jsx
+++ src/views/commodity/sellPriceModal/editableTable.jsx
@@ -10,11 +10,21 @@ import PropTypes from 'prop-types';
 import { Table } from 'antd';
 
 class EditableTable extends PureComponent {
+    state = {
+        prices: []
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (this.propsvalue.data.length === 0 && nextProps.value.data.length > 0) {
+            this.setState({ prices: nextProps.value.data });
+        }
+    }
+
     render() {
-        const { isEdit, startNumber } = this.props.value;
-        console.log(isEdit, startNumber);
+        // const { isEdit, startNumber } = this.props.value;
+        const { prices } = this.state;
         return (
-            <Table columns={[]} dataSource={[]} />
+            <Table columns={[]} dataSource={prices} />
         )
     }
 }
