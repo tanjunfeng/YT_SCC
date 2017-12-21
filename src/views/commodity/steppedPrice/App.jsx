147575e64c7f35b0fbff7a6dc107f013e8696a2d
tanjunfeng <tanjf@yatang.cn>
2017-12-21 17:02:17 +0800

    关系UI部分修改

--- src/views/commodity/steppedPrice/App.jsx
+++ src/views/commodity/steppedPrice/App.jsx
@@ -139,7 +139,7 @@ class SteppedPrice extends PureComponent {
             <div className={prefixCls}>
                 <div>
                     <div className={`${prefixCls}-title`}>
-                        <span className={`${prefixCls}-title-left`}>数量1111区间</span>
+                        <span className={`${prefixCls}-title-left`}>数量区间</span>
                         <span style={{paddingLeft: 150}}>售价 元/{initvalue}</span>
                         <span style={{paddingLeft: 55}}>商品毛利率</span>
                     </div>
