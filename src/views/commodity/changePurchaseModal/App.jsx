ef7a14189f8d6025bf9892a52c3385c50ffc3a89
ljy <30369113@qq.com>
2018-01-02 19:14:31 +0800

    采购审核查看开发

--- src/views/commodity/changePurchaseModal/App.jsx
+++ src/views/commodity/changePurchaseModal/App.jsx
@@ -287,8 +287,7 @@ class ProdModal extends Component {
                                             rules: [{ required: true, message: '采购内装数' }],
                                             initialValue: getProductByIds.purchaseInsideNumber
                                         })(
-                                            <InputNumber min={0} placeholder="内装数" />
-                                            )}
+                                            <InputNumber min={0} placeholder="内装数" />)}
                                     </span>
                                 </FormItem>
                                 {
@@ -300,8 +299,7 @@ class ProdModal extends Component {
                                                     rules: [{ required: true, message: '请输入当前采购价!' }],
                                                     initialValue: getProductByIds.purchasePrice
                                                 })(
-                                                    <InputNumber min={0} step={0.01} placeholder="当前采购价" />
-                                                    )}
+                                                    <InputNumber min={0} step={0.01} placeholder="当前采购价" />)}
                                             </span>
                                         </FormItem>
                                         : <FormItem>
@@ -311,8 +309,7 @@ class ProdModal extends Component {
                                                     rules: [{ required: true, message: '请输入采购价!' }],
                                                     initialValue: getProductByIds.purchasePrice
                                                 })(
-                                                    <InputNumber min={0} step={0.01} placeholder="采购价" />
-                                                    )}
+                                                    <InputNumber min={0} step={0.01} placeholder="采购价" />)}
                                             </span>
                                         </FormItem>
                                 }
@@ -325,8 +322,7 @@ class ProdModal extends Component {
                                                     rules: [{ required: true, message: '请输入最新采购价!' }],
                                                     initialValue: getProductByIds.newestPrice
                                                 })(
-                                                    <InputNumber min={0} step={0.01} placeholder="最新采购价" />
-                                                    )}
+                                                    <InputNumber min={0} step={0.01} placeholder="最新采购价" />)}
                                             </span>
                                             <span className={`${prefixCls}-adjustment`}>
                                                 调价百分比：{getProductByIds.percentage}%
@@ -340,7 +336,7 @@ class ProdModal extends Component {
                                         {getFieldDecorator('internationalCode', {
                                             rules: [{ required: true, message: '输入商品条码!' }],
                                             initialValue: isEdit ? internationalCode :
-                                            internationalCodes[0].internationalCode
+                                                internationalCodes[0].internationalCode
                                         })(
                                             <Select
                                                 placeholder="请选择商品条码"
@@ -357,8 +353,7 @@ class ProdModal extends Component {
                                                     )
                                                     )
                                                 }
-                                            </Select>
-                                            )}
+                                            </Select>)}
                                     </span>
                                 </FormItem>
                                 {
@@ -424,25 +419,25 @@ class ProdModal extends Component {
                                     <span className={`${prefixCls}-data-pic`}>
                                         <SearchMind
                                             defaultValue={
-                                            initValue.spAdrId &&
-                                            `${initValue.spAdrId} - ${initValue.spAdrName}`
+                                                initValue.spAdrId &&
+                                                `${initValue.spAdrId} - ${initValue.spAdrName}`
                                             }
                                             style={{ zIndex: 8 }}
                                             compKey="search-mind-key2"
                                             ref={ref => { this.searchMind2 = ref }}
                                             fetch={(params) =>
-                                            this.props.pubFetchValueList(Util.removeInvalid({
-                                                pId: this.ids.spId,
-                                                condition: params.value,
-                                                pageSize: params.pagination.pageSize,
-                                                pageNum: params.pagination.current || 1
-                                            }), 'supplierAdrSearchBox').then((res) => {
-                                                const dataArr = res.data.data || [];
-                                                if (!dataArr || dataArr.length === 0) {
-                                                    message.warning('没有可用的数据');
-                                                }
-                                                return res;
-                                            })}
+                                                this.props.pubFetchValueList(Util.removeInvalid({
+                                                    pId: this.ids.spId,
+                                                    condition: params.value,
+                                                    pageSize: params.pagination.pageSize,
+                                                    pageNum: params.pagination.current || 1
+                                                }), 'supplierAdrSearchBox').then((res) => {
+                                                    const dataArr = res.data.data || [];
+                                                    if (!dataArr || dataArr.length === 0) {
+                                                        message.warning('没有可用的数据');
+                                                    }
+                                                    return res;
+                                                })}
                                             onChoosed={this.handleAdressChoose}
                                             onClear={this.handleAdressClear}
                                             renderChoosedInputRaw={(res) => (
@@ -469,7 +464,7 @@ class ProdModal extends Component {
                                             className={`${prefixCls}-data-disable`}
                                             defaultValue={
                                                 (warehouseCode ||
-                                                initValue.distributeWarehouseId) &&
+                                                    initValue.distributeWarehouseId) &&
                                                 `${warehouseCode ||
                                                 initValue.distributeWarehouseId} - ${warehouseName ||
                                                 initValue.distributeWarehouseName}`}
@@ -521,8 +516,7 @@ class ProdModal extends Component {
                                             <Checkbox
                                                 checked={this.state.checked}
                                                 onChange={this.handleCheckBox}
-                                            />
-                                            )}
+                                            />)}
                                     </span>
                                 </FormItem>
                                 {
