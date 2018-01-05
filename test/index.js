96e111f270e74db9f82f968ac6e6731722118c02
taoqiyu <taoqiyu@yatang.cn>
2018-01-05 10:02:59 +0800

    测试加密文件

---
+++ test/index.js
@@ -0,0 +1,7 @@
+console.log('4.52' + '1');
+console.log('4.52' + '1');
+console.log('4.52' + '1');
+console.log('4.52' + '1');
+console.log('4.52' + '1');
+console.log('4.52' + '1');
+console.log('4.52' + '1');
