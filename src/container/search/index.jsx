7b926cdbeb89e489a4baf6a6e756946ccee3cd40
tanjunfeng <tanjf@yatang.cn>
2018-01-05 16:03:11 +0800

    merge

--- src/container/search/index.jsx
+++ src/container/search/index.jsx
@@ -2,7 +2,6 @@ import BranchCompany from './BranchCompany';
 import Franchisee from './Franchisee';
 import DirectStores from './DirectStores';
 import AddingGoodsByStore from './AddingGoodsByStore';
-import AddingGoodsByTerm from './AddingGoodsByTerm';
 import Supplier from './Supplier';
 import SupplierAdderss from './SupplierAddress';
 import Commodity from './Commodity';
@@ -12,7 +11,6 @@ export {
     Franchisee,
     DirectStores,
     AddingGoodsByStore,
-    AddingGoodsByTerm,
     Supplier,
     SupplierAdderss,
     Commodity
