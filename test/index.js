2b3480da34928dbcb33f6cf980bcc5e6cd983a0c
shijinhua <shijinhua@yatang.cn>
2018-01-05 10:12:10 +0800

    test

--- test/index.js
+++ test/index.js
@@ -1,7 +1,10 @@
 console.log('4.52' + '1');
+console.log('4.521' + '1');
+console.log('4.52' + '1');
+
 console.log('4.52' + '1');
 console.log('4.52' + '1');
 console.log('4.52' + '1');
 console.log('4.52' + '1');
-console.log('4.52' + '1');
-console.log('4.52' + '1');
+
+// shijh
