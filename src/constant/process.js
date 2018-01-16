6b0f11a4f7775987ea77ac04c60f44f9f1eabb4e
tanjunfeng <tanjf@yatang.cn>
2018-01-16 15:06:24 +0800

    预定专区

---
+++ src/constant/process.js
@@ -0,0 +1,17 @@
+// 预定专区处理状态
+export const processingState = {
+    defaultValue: '',
+    data: [{
+        key: '',
+        value: '全部'
+    }, {
+        key: 'init',
+        value: '待处理'
+    }, {
+        key: 'complete',
+        value: '已完成'
+    }, {
+        key: 'close',
+        value: '关闭'
+    }]
+}
