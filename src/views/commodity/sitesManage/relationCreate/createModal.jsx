34b071a3250f21390a0475b1f8ee7f6638d018b8
Zhou Changlong <zhouchanglong@yatang.net>
2018-01-29 17:18:56 +0800

    修复门店模糊查询功能

--- src/views/commodity/sitesManage/relationCreate/createModal.jsx
+++ src/views/commodity/sitesManage/relationCreate/createModal.jsx
@@ -104,6 +104,7 @@ class CreateModal extends PureComponent {
     }
 
     handPlaceTypeChange = val => {
+        const { setFieldsValue, resetFields } = this.props.form;
         /**
          * 根据地点类型，动态设置地点初始值
         */
@@ -128,6 +129,16 @@ class CreateModal extends PureComponent {
                 }
             });
         }
+
+        /**
+         * 切换类型，清空地点
+         */
+        setTimeout(() => {
+            setFieldsValue({
+                place: { reset: true }
+            });
+            resetFields(['place']);
+        });
     }
 
     /**
